f025d39263ddfb1879e7890a065c5665ec754ed4
Mikael Sauriol <mikaelsauriol5@hotmail.com>
2021-06-01 16:22:02 -0400

    kev c chenier

--- projet.R
+++ projet.R
@@ -2,6 +2,7 @@ library("tm")
 BiocManager::install("Biobase")
 install.packages('NMF')
 library(NMF)
+library(lsa)
 
 
 textes <- Corpus(DirSource("Textes", encoding = "UTF-8"), readerControl = list(language = "eng"))
@@ -11,6 +12,7 @@ textes <- tm_map(textes, tolower)
 textes <- tm_map(textes, removePunctuation)
 textes <- tm_map(textes, removeNumbers)
 textes <- tm_map(textes, removeWords, stopwords("english"))
+textes <- tm_map(textes, stemDocument, language = "english")
 
 length(stopwords("english"))
 stopwords("english")
@@ -20,18 +22,26 @@ nbDoc <- length(textes)
 X <- tdm
 
 #LSA
-R <- svd(X)
-U <- R$u
-V <- R$v
-S <- diag(R$d)
+R <- lsa(X,2)
+U <- R$tk
+V <- R$dk
+S <- diag(R$sk)
 
 plot(V[,1], V[,2], main= "Graph of documents", pch=20, col=1)
 text(V[,1], V[,2], seq(nbDoc), pos=2, col="red")
 
-VS <- V%*%S
+VS <- U%*%S%*%t(V)
+
+E <- sum((VS - X)^2)
 
 matrice <- as.matrix(rbind(tdm))
 #NMF
-P <- nmf(matrice, 2, method='brunet', nrun=10, seed=123456)
+P <- nmf(matrice, 2)
+W <- P@fit@W
+H <- P@fit@H
 
-profplot(P)
+plot(H[1,], H[2,], main= "Graph of documents", pch=20, col=1)
+text(H[1,], H[2,], seq(nbDoc), pos=2, col="red")
+
+WH <- W%*%H
+Ensa <- sum((WH - X)^2)
